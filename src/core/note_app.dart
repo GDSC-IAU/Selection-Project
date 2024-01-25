554bf37ff44b3926f48fc25111caf32659257d0f
arwaalkhathlan <arwa124344@gmail.com>
2024-01-25 12:34:17 +0300

    Add task management functionality to NoteApp

--- src/core/note_app.dart
+++ src/core/note_app.dart
@@ -1,6 +1,111 @@
+import 'dart:io';
+import './utils.dart';
+
 class NoteApp {
   // TODO: Complete the run function
   void run() {
-    print('Hello, world!');
-  }
+     List<TaskListApp> tasks = [];
+  print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+  print('Task List App');
+
+  var answer;
+
+
+  do {
+    print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+    print('What would you like to do?');
+    print('1. Add task');
+    print('2. Edit task');
+    print('3. Delete task');
+    print('4. View tasks');
+    print('5. Exit');
+
+    try {
+      answer = int.parse(stdin.readLineSync()!);
+    } catch (e) {
+      print('Invalid input. Please enter a number.');
+    }
+    print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+
+    
+    String? answer1;
+    switch (answer) {
+      case 1:
+
+      
+
+      do{
+
+        addTask(tasks);
+         print('Do you want to add another task? Please answer with yes or no:');
+         print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+          answer1 = stdin.readLineSync()?.toLowerCase();
+      } while (answer1 == 'yes' || answer1 == 'y');
+
+        break;
+
+      case 2:
+        print('Edit task');
+        print('Enter the task ID you want to edit:');
+        int taskId = int.parse(stdin.readLineSync()!);
+
+        TaskListApp? taskToEdit =
+            tasks.firstWhereOrNull((task) => task.id == taskId);
+
+        if (taskToEdit != null) {
+
+          print('What would you like your new task name to be?');
+          taskToEdit.task_name = stdin.readLineSync()!;
+
+          print('Enter the new task description:');
+          taskToEdit.description = stdin.readLineSync()!;
+
+          print('Is you task completed:');
+          String? input = stdin.readLineSync()?.trim().toLowerCase();
+          bool isCompleted = input == 'y' || input == 'yes';
+
+          print('Task edited successfully!!');
+        } else {
+          print('Task not found with ID $taskId :(');
+        }
+        print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+        break;
+
+      case 3:
+        print('Delete task');
+        print('Enter the task ID you want to delete:');
+        int taskIdToDelete = int.parse(stdin.readLineSync()!);
+
+        int initialLength = tasks.length;
+        tasks.removeWhere((task) => task.id == taskIdToDelete);
+
+        if (tasks.length < initialLength) {
+          print('Task deleted successfully!!');
+        } else {
+          print('Task not found with ID $taskIdToDelete :(');
+        }
+        print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+        break;
+
+      case 4:
+        print('Here is your tasks:');
+        for (var task in tasks) {
+          print(task);
+        }
+        print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+        break;
+
+      case 5:
+        print('Exit');
+        print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+
+        break;
+
+      default:
+        print('Invalid input');
+        print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+        break;
+    }
+  } while (answer != 5);
+}
 }
--- src/core/utils.dart
+++ src/core/utils.dart
@@ -1,1 +1,81 @@
 // TODO: Add any utility functions here
+
+import 'dart:io';
+
+class TaskListApp {
+  int id;
+  String task_name;
+  String description;
+  bool isCompleted;
+
+  // Default constructor
+  TaskListApp()
+      : this.withDetails(
+            id: 0, task_name: '', description: '', isCompleted: false);
+
+  // Constructor with named parameters
+  TaskListApp.withDetails({
+    required this.id,
+    required this.task_name,
+    required this.description,
+    required this.isCompleted,
+  });
+
+  @override
+  String toString() {
+    String finished = isCompleted ? 'Yes' : 'No';
+    print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+    return 'Task ID: $id \nTaskName: $task_name \nDescription: $description \nFinished: $finished';
+
+  }
+}
+
+extension IterableExtensions<T> on Iterable<T> {
+  T? firstWhereOrNull(bool Function(T) test) {
+    try {
+      return firstWhere(test);
+    } catch (e) {
+      return null;
+    }
+  }
+}
+
+void addTask(List<TaskListApp> tasks) {
+
+    print('What would you like your task name to be?');
+    String? task_name;
+
+    //to check if it's empty also asks again
+    do {
+      task_name = stdin.readLineSync();
+      if (task_name!.isEmpty) {
+        print("Task_name can't be empty. Please enter a Task name:");
+      }
+    } while (task_name.isEmpty);
+
+    print('Enter your task description:');
+    String? description;
+
+
+    //to check if it's empty also asks again
+    do {
+      description = stdin.readLineSync();
+      if (description!.isEmpty) {
+        print("Description cannot be empty. Please enter a description:");
+      }
+    } while (description.isEmpty);
+
+    print('Is your task completed:');
+    String? input = stdin.readLineSync()?.trim().toLowerCase();
+    bool isCompleted = input == 'y' || input == 'yes';
+    print("♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡");
+
+    TaskListApp task = TaskListApp.withDetails(
+      id: tasks.length + 1,
+      task_name: task_name,
+      description: description,
+      isCompleted: isCompleted,
+    );
+
+    tasks.add(task);
+}
